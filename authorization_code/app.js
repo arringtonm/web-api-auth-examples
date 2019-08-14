52891ca893a32bf3074fa2743fc115fe3ba4d60a
Michael Arrington <michaelarringtoniphone@gmail.com>
2019-08-13 21:16:34 -0700

    Add recursive funct to grab ID of each track

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -14,8 +14,7 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
 
-var client_id = ''; // Your client id
-var client_secret = ''; // Your secret
+
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
